e836549f0fcd070eaf79ac2ff941df065f9b8085
0xLLLLH <linlihao159@gmail.com>
2024-04-10 02:37:17 +0800

    chore(release): v0.0.5

--- src/promise.ts
+++ src/promise.ts
@@ -1,19 +1,19 @@
-export const resolveAfter = async <T = unknown>(timeInMs: number, resolveValue?: T) => {
-  return new Promise(resolve => {
-      setTimeout(() => {
-          resolve(resolveValue);
-      }, timeInMs);
+export async function resolveAfter<T = unknown>(timeInMs: number, resolveValue?: T) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(resolveValue)
+    }, timeInMs)
   })
 }
 
-export const rejectAfter = async <T = unknown>(timeInMs: number, rejectValue?: T) => {
+export async function rejectAfter<T = unknown>(timeInMs: number, rejectValue?: T) {
   return new Promise((_, reject) => {
-      setTimeout(() => {
-          reject(rejectValue);
-      }, timeInMs);
+    setTimeout(() => {
+      reject(rejectValue)
+    }, timeInMs)
   })
 }
 
-export const waitFor = async (timeInMs: number) => {
+export async function waitFor(timeInMs: number) {
   await resolveAfter(timeInMs)
 }
